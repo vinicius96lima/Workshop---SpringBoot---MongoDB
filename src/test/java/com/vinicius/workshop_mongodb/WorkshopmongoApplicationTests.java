4231230a9ecc38c1a32268796806a9e2462bf2c3
vinicius96lima <viniciusaraujo_96@hotmail.com>
2023-04-18 11:14:08 -0300

    Projeto criado

---
+++ src/test/java/com/vinicius/workshop_mongodb/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.workshop_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
