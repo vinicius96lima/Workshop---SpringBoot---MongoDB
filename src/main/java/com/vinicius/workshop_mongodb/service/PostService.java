6e379575aae160d59d9d6a9fdbe69eb6cd09d30e
vinicius96lima <viniciusaraujo_96@hotmail.com>
2023-04-19 12:16:14 -0300

    Busca post por Id

---
+++ src/main/java/com/vinicius/workshop_mongodb/service/PostService.java
@@ -0,0 +1,30 @@
+package com.vinicius.workshop_mongodb.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vinicius.workshop_mongodb.domain.Post;
+import com.vinicius.workshop_mongodb.repositor.PostRepository;
+import com.vinicius.workshop_mongodb.service.exception.ObjectNotFound;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	// @Autowired faz a função de procura da classe intanciando o objeto;
+	
+	public List <Post> findAll(){
+		return repo.findAll();
+		
+	}
+	
+	public Post findById (String id) {
+		Optional<Post> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFound("Objeto nao encontrado"));
+	}
+	
+}
